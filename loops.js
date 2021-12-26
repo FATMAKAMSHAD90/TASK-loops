52b8af4b9d7c9ab7dbc1993c06f223b72a90e441
FATMAKAMSHAD90 <fatmakamshad90@gmail.com>
2021-12-26 14:48:06 +0300

    done

--- loops.js
+++ loops.js
@@ -9,8 +9,11 @@
   # 
  */
 function drawVLine(n) {
-  // Continue the code here
+  for (let i = 0; i < 5; i++) {
+    console.log("#");
+  }
 }
+drawVLine(5);
 /**
  *
  * In this function, you will have to draw a horizontal line with `n` length
@@ -18,64 +21,107 @@ function drawVLine(n) {
  */
 function drawHLine(n) {
   // Continue the code here
+  for (let i = 0; i < n; i++) {
+    process.stdout.write("#");
+  }
+  console.log("#");
 }
 
-/**
- * 
- * In this function, you will have to draw a square with a length of `n` 
- * for example `drawSquare(5) should print out 
- 
-  #  #  #  #  #
-  #  #  #  #  #
-  #  #  #  #  #
-  #  #  #  #  #
-  #  #  #  #  #
+drawHLine(5);
 
- */
+/**
+   * 
+   * In this function, you will have to draw a square with a length of `n` 
+   * for example `drawSquare(5) should print out 
+   
+    #  #  #  #  #
+    #  #  #  #  #
+    #  #  #  #  #
+    #  #  #  #  #
+    #  #  #  #  #
+  
+   */
 function drawSquare(n) {
   // Continue the code here
+  for (let j = 0; j < n; j++) {
+    for (let i = 0; i < n; i++) {
+      process.stdout.write("#");
+    }
+    console.log("#");
+  }
 }
-
+drawSquare(5);
 /**
- * 
- * In this function, you will have to draw a triangle with a length of `n` 
- * for example `drawLeftTriangle(5) should print out 
- 
-  #  
-  #  #  
-  #  #  #  
-  #  #  #  #  
-  #  #  #  #  #
-
- */
+   * 
+   * In this function, you will have to draw a triangle with a length of `n` 
+   * for example `drawLeftTriangle(5) should print out 
+   
+    #  
+    #  #  
+    #  #  #  
+    #  #  #  #  
+    #  #  #  #  #
+  
+   */
 function drawLeftTriangle(n) {
   // Continue the code here
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      process.stdout.write("# ");
+    }
+    console.log("#");
+  }
 }
-
+drawLeftTriangle(5);
 /**
- * 
- * In this function, you will have to draw a tree with a length of `n` 
- * for example `drawTree(5) should print out 
- 
-     #   
-    # #   
-   # # # 
-  # # # # 
-     #
-
- */
+   * 
+   * In this function, you will have to draw a tree with a length of `n` 
+   * for example `drawTree(5) should print out 
+   
+       #   
+      # #   
+     # # # 
+    # # # # 
+       #
+  
+   */
 function drawTree(n) {
   // Continue the code here
-}
-/**
- *
- * In this example, you will create 4 arrays, `yellowCards`, `greenCards`, `redCards`, `blueCards`
- * (BONUS) Try to optimize the code with doing it with one gigantic loop
- */
-function fillUnoDeck() {
-  let greenCards = [];
-  let yellowCards = [];
-  let redCards = [];
-  let blueCards = [];
-  // Fill the cards here
+  //row
+  for (let i = 0; i < n; i++) {
+    //space
+    for (let s = n - 1; s >= i; s--) {
+      process.stdout.write("# ");
+      //column
+      for (let j = 0; j < i; j++) {
+        process.stdout.write("# ");
+      }
+      console.log("#");
+    }
+    for (let o = 0; o < n; o++) {
+      process.stdout.write("# ");
+    }
+  }
+  drawTree(5);
+  /**
+   *
+   * In this example, you will create 4 arrays, `yellowCards`, `greenCards`, `redCards`, `blueCards`
+   * (BONUS) Try to optimize the code with doing it with one gigantic loop
+   */
+  function fillUnoDeck() {
+    let greenCards = [];
+    let yellowCards = [];
+    let redCards = [];
+    let blueCards = [];
+    // Fill the cards here
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 2; j++) {
+        greenCards.push(i);
+        yellowCards.push(i);
+        redCards.push(i);
+        blueCards.push(i);
+      }
+      console.log();
+    }
+  }
 }
